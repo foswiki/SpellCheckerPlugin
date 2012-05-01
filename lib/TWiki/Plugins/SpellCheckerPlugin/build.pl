b8ba16712652c07941eae8c272bf87c75626a7e5
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/SpellCheckerPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/SpellCheckerPlugin/build.pl
+++ lib/TWiki/Plugins/SpellCheckerPlugin/build.pl
@@ -3,14 +3,14 @@
 # Build for SpellCheckerPlugin
 #
 BEGIN {
-    unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS});
+    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
 
 use TWiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build( 'SpellCheckerPlugin' );
+$build = new TWiki::Contrib::Build('SpellCheckerPlugin');
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 
